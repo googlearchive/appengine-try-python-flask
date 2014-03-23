e3a646efcc9c8355ee7f1894d6036efe3f316b99
Dan Sanderson <contact@dansanderson.com>
2014-03-22 18:30:10 -0700

    Slimming down the Flask sample.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-"""`main` is the top level module for your Flask application."""
-
-# Import the Flask Framework
 from flask import Flask
 app = Flask(__name__)
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
@@ -16,4 +14,4 @@ def hello():
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
+    return 'Sorry, nothing at this URL.', 404
