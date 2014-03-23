24bcb3bbc7ff4c7a398e4e526357fe533b5e1de3
Fred Sauer <fredsa@google.com>
2014-03-23 10:05:26 -0700

    show useful tracebacks when something goes wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
