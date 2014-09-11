d2da59eca774063d678a8b8314ba0921ecc3d22e
Michael R. Bernstein <michael@fandomhome.com>
2014-09-11 16:06:11 -0500

    Changed the import style.

--- appengine_config.py
+++ appengine_config.py
@@ -1,8 +1,7 @@
 """`appengine_config` gets loaded when starting a new application instance."""
-from vendor import insertsitedir
+import vendor
 import os.path
 # add `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions
-insertsitedir(1, os.path.join(os.path.dirname(__file__), 'lib'))
-
+vendor.insertsitedir(1, os.path.join(os.path.dirname(__file__), 'lib'))
