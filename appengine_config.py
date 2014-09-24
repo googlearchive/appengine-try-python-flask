4dccb2053ce1bf6c9951f03e1375b90b1c629fb5
Michael Bernstein <michael@fandomhome.com>
2014-09-24 07:33:07 -0500

    made latest changes for consistency with Darth-Vendor.

--- appengine_config.py
+++ appengine_config.py
@@ -1,7 +1,6 @@
 """`appengine_config` gets loaded when starting a new application instance."""
 import vendor
-import os.path
 # add `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions
-vendor.insertsitedir(1, os.path.join(os.path.dirname(__file__), 'lib'))
+vendor.insertsitedir('lib')
--- vendor.py
+++ vendor.py
@@ -1,9 +1,51 @@
+import os
 import site
 import sys
 
-def insertsitedir(index, path):
-    """Insert a site directory at the specified index of the path."""
+
+def insertsitedir(directory, index=1):
+    """
+    Adds the given folder to the python path. Supports namespaced packages.
+    By default, packages in the given folder take precedence over site-packages
+    and any previous path manipulations.
+
+    Args:
+    directory: Path to the directory containing packages, relative to the
+    current working directory.
+    index: Where in ``sys.path`` to insert the vendor packages. By default
+    this is set to 1. It is inadvisable to set it to 0 as it will override
+    any modules in the current working directory.
+    """
+    # Use site.addsitedir() because it appropriately reads .pth
+    # files for namespaced packages. Unfortunately, there's not an
+    # option to choose where addsitedir() puts its paths in sys.path
+    # so we have to do a little bit of magic to make it play along.
+
+    # We're going to grab the current sys.path and split it up into
+    # the first entry and then the rest. Essentially turning
+    #   ['.', '/site-packages/x', 'site-packages/y']
+    # into
+    #   ['.'] and ['/site-packages/x', 'site-packages/y']
+    # The reason for this is we want '.' to remain at the top of the
+    # list but we want our vendor files to override everything else.
     sys.path, remainder = sys.path[:index], sys.path[index:]
-    site.addsitedir(path)
+
+    # Now we call addsitedir which will append our vendor directories
+    # to sys.path (which was truncated by the last step.)
+    site.addsitedir(directory)
+
+    # Finally, we'll add the paths we removed back.
     sys.path.extend(remainder)
 
+
+def insertvenv(path, index=1):
+    """
+    Adds the given virtualenv's site-packages to the python path.
+
+    Args:
+    path: Path to a root of the virtualenv, relative to ``os.getcwd()``
+    index: passed through to insertsitedir().
+    """
+    site_dir = os.path.join(path, 'lib', 'python' + sys.version[:3],
+                            'site-packages')
+    insertsitedir(site_dir, index=index)
